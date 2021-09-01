3bb94ac00c1d942f2a0e57111bc20f78ae7b5d12
luisluft <luberluft@gmail.com>
2021-09-01 10:34:37 -0400

    implement operator display feature

--- script.js
+++ script.js
@@ -2,17 +2,44 @@ const calculatorDisplay = document.querySelector("h1");
 const inputButtons = document.querySelectorAll("button");
 const clearButton = document.querySelector(".clear-button");
 
-function sendNumberValue(number) {
-  const displayedValue = calculatorDisplay.textContent;
+let firstValue = 0;
+let operatorValue = "";
+let operatorPressed = false;
 
-  calculatorDisplay.textContent = displayedValue === "0" ? number : displayedValue + number;
+function sendNumberValue(number) {
+  if (operatorPressed) {
+    calculatorDisplay.textContent = number;
+    operatorPressed = false;
+  } else {
+    const displayedValue = calculatorDisplay.textContent;
+    calculatorDisplay.textContent = displayedValue === "0" ? number : displayedValue + number;
+  }
+}
+
+function useOperator(operator) {
+  const currentValue = Number(calculatorDisplay.textContent);
+  operatorValue = operator;
+
+  // First value
+  if (!firstValue) firstValue = currentValue;
+  else console.log("currentValue :", currentValue);
+  // Second value
+  operatorPressed = true;
+
+  console.log("firstValue :", firstValue);
+  console.log("operatorValue :", operatorValue);
 }
 
 function resetCalculator() {
+  firstValue = 0;
+  operatorValue = "";
+  operatorPressed = false;
   calculatorDisplay.textContent = "0";
 }
 
 function addDecimal() {
+  // Do nothing if operator was already pressed
+  if (operatorPressed) return;
   // First decimal
   if (!calculatorDisplay.textContent.includes(".")) calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
 }
@@ -22,7 +49,7 @@ inputButtons.forEach((button) => {
   // Numbered buttons
   if (button.classList.length === 0) button.addEventListener("click", () => sendNumberValue(button.value));
   // Operator buttons
-  else if (button.classList.contains("operator")) button.addEventListener("click", () => sendNumberValue(button.value));
+  else if (button.classList.contains("operator")) button.addEventListener("click", () => useOperator(button.value));
   // Decimal buttons
   else if (button.classList.contains("decimal")) button.addEventListener("click", addDecimal);
 });
